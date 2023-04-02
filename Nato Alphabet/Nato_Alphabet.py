f80fd972d4600781acc0a0b7ca7cccef15753b4c
AlfonsoAlvarez <lualar@gmail.com>
2023-04-02 17:55:47 -0500

    With Error Exception and Loop

--- Nato Alphabet/Nato_Alphabet.py
+++ Nato Alphabet/Nato_Alphabet.py
@@ -1,6 +1,6 @@
+from pickle import TRUE
 import pandas as pd
 import datetime 
-
 
 #Dict comprehension from PandasDataframe
 #t1 = datetime.datetime.now()
@@ -11,13 +11,20 @@ dNatoAlphabet2 = {row.letter:row.code for index, row in dfNato.iterrows()}
 #print (f"Method 1 Time: {delta.microseconds} seconds")
 #print (f"NATO Alphabet: {dNatoAlphabet2}")
 
-#ask for a word
-sUserInput = input("\nEnter a single word:")
-sUserInput = sUserInput.replace(" ", "").upper()
+sExit = False
+while sExit == False:
+    try:
+        #ask for a word
+        sUserInput = input("\nEnter a single word:")
+        sUserInput = sUserInput.replace(" ", "").upper()
  
-#Method 1 (fastest -- correct)
-lWords = [dNatoAlphabet2[letter] for letter in sUserInput]
-print(f"\nMethod 1: {lWords}")
+        #Method 1 (fastest -- correct)
+        lWords = [dNatoAlphabet2[letter] for letter in sUserInput]
+        sExit = True
+    except BaseException:
+        lWords  = "Sorry, only letters in the word please."
+    else:
+        print(f"\nNato Alphabet Results: {lWords}")
 
 
 
